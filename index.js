47c71253a3e2f22a030c707f384286072f499c29
amalms47 <amal@technorip.com>
2018-07-27 11:53:27 +0530

    mongo operations first commit final

--- index.js
+++ index.js
@@ -1,18 +1,105 @@
-const express = require('express');
-const app=express();
-const Joi=require('joi');
-const home=require('./routes/home');
-const coursepage=require('./routes/courses');
+const mongo=require('mongoose');
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true})); //key:value
-app.use(express.static('public'));
-app.use('/',home);
-app.use('/api',coursepage);
+//MongoDB connection 
 
-app.set('view engine','pug');
-app.set('views','./views');
+mongo.connect('mongodb://localhost/playground')
+.then(()=>console.log('connected to db'))
+.catch(err=>console.log('could not connect',err));
+
+//Schema
+
+const courseSchema=new mongo.Schema({
+    name:String,
+    author:String,
+    tags:[String],                              // represents array
+    date:{type:Date,default:Date.now},
+    isPublished:Boolean
+})
 
 
-const port=process.env.port||3000;
-app.listen(3000,() => console.log(`Listening to port ${port}`));
+
+// Creating models
+
+const Course=mongo.model('Course',courseSchema);  //creating a class called Course
+
+
+async function createCourse() {
+    
+ const course=new Course({                            //creating object course  of class Course 
+    name:'Angular.js course',
+    author:'Anoop',
+    tags:['Angular','Frontend'],
+    isPublished:true
+ });
+
+ const result=await course.save();
+ console.log(result);
+
+}
+
+
+// get courses
+
+async function getCourses(){
+
+
+
+
+                 /*******************    Logical query operators  *****************/
+
+
+
+
+    // or                                //after .find() .or([ {author:'Amal'},{isPublished:true }])  
+    // and
+
+
+
+                 /*******************    comparison query operators  *****************/
+
+    // eq (equal)
+    // ne (not equal)
+    // gt (greater than)                 // .find({price : $gt:10})       
+    // gte (grreater than or equal)                                                   
+    // lt (lessathan) 
+    // lte (lessathan or equal) 
+    // in                               // .find({price : $in:[2,20,4,34]})
+    // nin (not in)
+
+
+
+
+                 /*******************    Regular expressions  *****************/
+
+
+    //Starts with a name Amal
+    
+    // =>    .find({author:/^Amal/})
+
+     //Ends with a name Amal
+
+     // =>    .find({author:/Amal$/})    to avoid case sensitive add i => /Amal$/i
+
+     //Contains Amal
+     
+    //=>     .find({author:/.*Amal.*/})
+
+
+    const pageNumber=2;
+    const pageSize=10;
+
+    const course=await Course
+
+    .find()                                                         //use .find() to get  all
+    .skip((pageNumber-1)*pageSize)                                  // pagination
+    .limit(pageSize)                                                  
+    .sort({name:1})                                            // 1  for ascending -1 for descebding
+    //.select({name:1,tags:1,author:1});                                               // select required field
+
+    //.count();                                                   // return count
+    console.log(course);
+}
+
+//createCourse();
+
+getCourses();
